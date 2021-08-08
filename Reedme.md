6a157d661c6181df329f6b6852e48d767e835e1d
pepsi999cola <edge999standard@gmail.com>
2021-08-08 18:48:49 +0900

    first commit

---
+++ Reedme.md
@@ -0,0 +1,10 @@
+# ユーチューブから動画をダウンロード
+
+個別にDLしたいときはdownload.pyを使う  
+チャンネル毎にDLしたいときはchannel_download.pyを使う  
+
+## install
+
+```python
+python -m pip install pytube
+```
---
+++ channel_download.py
@@ -0,0 +1,7 @@
+from pytube import Channel
+
+c = Channel('https://www.youtube.com/channel/UCLjzHC8I2T99kuxtaaZLaTQ/videos') #channel_urlを入力
+print(f'Downloading videos by: {c.channel_name}')
+for video in c.videos:
+     video.streams.filter(progressive=True, file_extension='mp4').desc().order_by('resolution').first().download()
+     print(video.title)
