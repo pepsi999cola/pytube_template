7e0638f1a825436479d9265129b5e38ae6903127
pepsi999cola <edge999standard@gmail.com>
2021-09-18 23:47:56 +0900

    httpsに変更

--- download.py
+++ download.py
@@ -1,6 +1,6 @@
 from pytube import YouTube
 
-yt = YouTube('http://youtube.com/watch?v=') #動画のurlを入力
+yt = YouTube('https://www.youtube.com/watch?v=') #動画のurlを入力
 yt.streams.filter(progressive=True, file_extension='mp4').desc().order_by('resolution').first().download()
 # caption = yt.captions.get_by_language_code('en')
 # caption = caption.xml_captions
