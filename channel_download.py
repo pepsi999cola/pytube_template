2380884bb28343e1be0209cea90fc586960ea066
pepsi999cola <edge999standard@gmail.com>
2021-09-20 19:33:16 +0900

    コーディング規約変更

--- channel_download.py
+++ channel_download.py
@@ -1,8 +1,15 @@
 from pytube import Channel
 
-c = Channel('https://www.youtube.com/channel/')  # channel_urlを入力
+# channel_urlを入力
+c = Channel('https://www.youtube.com/channel/UCfvZempyFTpPlklLojBj0iQ/videos')
+
 print(f'Downloading videos by: {c.channel_name}')
 for video in c.videos:
-    video.streams.filter(progressive=True, file_extension='mp4').desc().order_by(
-        'resolution').first().download()
+    (video.streams
+          .filter(progressive=True, file_extension='mp4')
+          .desc()
+          .order_by('resolution')
+          .first()
+          .download()
+     )
     print(video.title)
--- download.py
+++ download.py
@@ -1,7 +1,15 @@
 from pytube import YouTube
 
-yt = YouTube('https://www.youtube.com/watch?v=') #動画のurlを入力
-yt.streams.filter(progressive=True, file_extension='mp4').desc().order_by('resolution').first().download()
+yt = YouTube('https://www.youtube.com/watch?v=pjSz0UkS5SU')  # 動画のurlを入力
+
+(yt.streams
+    .filter(progressive=True, file_extension='mp4')
+    .desc()
+    .order_by('resolution')
+    .first()
+    .download()
+ )
+
 # caption = yt.captions.get_by_language_code('en')
 # caption = caption.xml_captions
 print(yt.title)
