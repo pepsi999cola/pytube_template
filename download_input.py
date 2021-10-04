a37c1b6f753808c5bb3206e78dc652e5d4f7744e
pepsi999cola <edge999standard@gmail.com>
2021-10-04 15:44:43 +0900

    字幕

--- download_input.py
+++ download_input.py
@@ -3,6 +3,10 @@ from pytube import YouTube
 url = input('URLを入力してください:')
 yt = YouTube(url)  # 動画のurlを入力
 
+#字幕
+# caption = yt.captions.get_by_language_code('a.ja')
+# print(caption.xml_captions)
+
 (yt.streams
     .get_highest_resolution()
     .download()
