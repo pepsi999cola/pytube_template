4da8d426111f86cdad9430eb6e68f5b13de8cdd2
pepsi999cola <edge999standard@gmail.com>
2021-10-20 12:22:20 +0900

    channelとplaylistをDL時にディレクトリを作成

--- channel_download_input.py
+++ channel_download_input.py
@@ -1,3 +1,4 @@
+import os
 from pytube import Channel
 
 # channel_urlを入力
@@ -5,10 +6,11 @@ url = input('YoutubechannelのURLを入力してください:')
 c = Channel(url)
 
 print(f'Downloading videos by: {c.channel_name}')
+os.makedirs(c.channel_name, exist_ok=True)
 
 for video in c.videos:
     (video.streams
         .get_highest_resolution()
-        .download()
+        .download(c.channel_name)
     )
     print(video.title)
--- playlist_download.py
+++ playlist_download.py
@@ -1,3 +1,4 @@
+import os
 from pytube import Playlist
 
 url = input('URLを入力してください:')
@@ -5,10 +6,11 @@ p = Playlist(url)
 
 print(f'Downloading: {p.title}')
 print(p.length)
+os.makedirs(p.title, exist_ok=True)
 
 for video in p.videos:
     (video.streams
         .get_highest_resolution()
-        .download()
+        .download(p.title)
     )
     print(video.title)
